716c880502e8a87b000f980de205c7ee98a0aece
Toby Dawson <tobydawson1@gmail.com>
2020-06-03 14:58:19 +0100

    request for title, calories and imag

--- src/App.js
+++ src/App.js
@@ -29,7 +29,10 @@ const App = () => {
         Search </button>
       </form>
       {recipes.map(recipe =>(
-        <Recipe />
+        <Recipe 
+          title={recipe.recipe.label}
+          calories={recipe.recipe.calories} 
+          image={recipe.recipe.image} />
       ))};
     </div>
   )
